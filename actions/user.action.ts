8bad4eb422519154249b6c2d9b3b591bd361cb81
Adrian Calicdan <adriancalicdan2@gmail.com>
2024-11-29 21:44:45 +0800

    update

---
+++ actions/user.action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import User from "../modals/user.modal";
+import { connect } from "../db";
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    await newUser.save();
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
